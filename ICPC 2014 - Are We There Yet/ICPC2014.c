af09d4bd9c04fd7c6e556b2ae6abb47ec598a33c
stbeneventi <stephano@23people.io>
2021-03-17 12:34:00 -0300

    Refactored ICPC2014

--- ICPC 2014 - Are We There Yet/ICPC2014.c
+++ ICPC 2014 - Are We There Yet/ICPC2014.c
@@ -1,18 +1,19 @@
 /*******************************************************************************
  * 2014 - Are We There Yet?
- * https://icpcarchive.ecs.baylor.edu/index.php?option=com_onlinejudge&page=show_problem&problem=15
+ *
+ * Url:
+ *   https://icpcarchive.ecs.baylor.edu/index.php?option=com_onlinejudge&page=show_problem&problem=15
  *
  * Author:
  *   Stephano Beneventi <stbeneventi@gmail.com>
  *
- * Description:
- *   You have a number that represents the location of a cookie, and a kid moves
- * from one position to another, if he gets closer, the program should print
- * "warmer.", if he gets away "colder.", and if doesn't move "same.", and "found
- * it!" if he reach the cookie. There's an extra difficulty, because there's
- * only extra line breaks between the differents outputs but none in the end.
- * The weird thing is that ACM evaluation tool, throws an "Wrong Answer",
- * instead of "Presentation Error".
+ * Notes:
+ *   The difficulty of this problem is the variable number of integers of the
+ * input because the scanf function ignores newline characters, so the solution
+ * is to use a reader that allows to read newline characters.
+ * Note that there's two linebreaks between every case, but there's only one
+ * after the final case. If that's incorrect, the evaluation tool throws a
+ * "Wrong Answer", instead of a "Presentation Error".
  *
  */
 
@@ -20,48 +21,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char* getToken(char delim, char* index, bool skipDelim) {
+char skipInitialDelimiters(delimiter) {
   char c = getchar();
-  if (c == EOF) {
-    *index = EOF;
-    return NULL;
-  }
-  while (skipDelim && c == delim) {
+  while (c == delimiter) c = getchar();
+  return c;
+}
+
+char *readTokenFromStdin(bool *linebreak) {
+  char delimiter = ' ';
+
+  int indexChars = 0;
+  int tokenLength = 8;
+  char *token = (char *)malloc(tokenLength * sizeof(char));
+  char c = skipInitialDelimiters(delimiter);
+  while (c != '\n' && c != EOF && c != delimiter) {
+    if (indexChars >= tokenLength - 1)
+      token = (char *)realloc(token, (tokenLength *= 2) * sizeof(char));
+    token[indexChars++] = c;
     c = getchar();
-    if (c == EOF) {
-      *index = EOF;
-      return NULL;
-    }
-  }
-  int length = 8;
-  char* token = malloc(length * sizeof(char));
-  int count = 0;
-  while (c != '\n' && c != EOF && c != delim) {
-    if (count >= length) {
-      token = realloc(token, (length += length) * sizeof(char));
-    }
-    token[count++] = c;
-    c = getchar();
-  }
-  if (count >= length) {
-    token = realloc(token, (length += 1) * sizeof(char));
-  }
-  token[count] = '\0';
-  *index = c;
+  };
+  token[indexChars] = '\0';
+  linebreak = (c == '\n');
   return token;
 }
 
-void skipLine(char* index) {
-  while (*index != '\n' && *index != EOF) {
-    *index = getchar();
-  }
-}
-
-bool printMove(int cookiePos, int currPos, int nextPos) {
+bool move(int cookiePos, int currPos, int nextPos) {
   printf("Moving from %d to %d: ", currPos, nextPos);
   if (cookiePos == nextPos) {
     printf("found it!\n");
-    return true;
+    return false;
   } else {
     int diff1 = abs(cookiePos - nextPos);
     int diff2 = abs(cookiePos - currPos);
@@ -74,31 +62,24 @@ bool printMove(int cookiePos, int currPos, int nextPos) {
       printf("same.\n");
     }
   }
-  return false;
+  return true;
 }
 
 int main() {
-  bool newLineFlag = false;
-  char index;
-  while (index != EOF) {
-    int cookiePos = atoi(getToken(' ', &index, true));
-    int currPos = 0;
-    if (cookiePos >= 5280 || cookiePos <= -5280) {
-      break;
+  bool firstCase = true;
+  bool linebreak = false;
+  int cookiePosition = atoi(readTokenFromStdin(&linebreak));
+  while (cookiePosition < abs(5280)) {
+    if (!firstCase) printf("\n");
+    int currentPosition = 0;
+    int nextPosition = atoi(readTokenFromStdin(&linebreak));
+    while (!linebreak) {
+      if (!move(cookiePosition, currentPosition, nextPosition)) break;
+      currentPosition = nextPosition;
+      nextPosition = atoi(readTokenFromStdin(&linebreak));
     }
-    if (newLineFlag) {
-      printf("\n");
-    } else {
-      newLineFlag = true;
-    }
-    while (index != '\n' && index != EOF) {
-      int nextPos = atoi(getToken(' ', &index, true));
-      if (printMove(cookiePos, currPos, nextPos)) {
-        break;
-      };
-      currPos = nextPos;
-    }
-    skipLine(&index);
+    cookiePosition = atoi(readTokenFromStdin(&linebreak));
+    firstCase = false;
   }
 
   return 0;
